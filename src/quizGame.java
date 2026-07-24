f3f3bc3b6e48f051499b302dc65ca76ff0ee5753
Solo-Men <learnitall66@gmail.com>
2026-07-24 23:02:06 +0530

    Add a Quiz Game

---
+++ src/quizGame.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class quizGame {
+    public static void main(String[] args){
+        //Java Quiz Game
+
+        //QUESTIONS ARRAY
+        String[] questions  = {"what is the main function of a router?",
+                               "Which part of the computer is considered the brain?",
+                               "What year was facebook launched?",
+                               "Who is known as the father of computer?",
+                               "What was the first programming language?"};
+        //OPTIONS ARRAY[][]
+        String[][] options = {{"1.Storing files", "2.Encrypting data", "3.Directing Internet traffic", "4.Managing passwords"},
+                              {"1.CPU ","2.Hard Drive ","3.RAM ","4.GPU "},
+                              {"1.2000 ","2.2004 ","3.2006 ","4.2008 "},
+                              {"1.Steve Jobs ","2.Bill Gates ","3.Alan Turing ","4.Charles Babbage "},
+                              {"1.COBOL ","2.C ","3.Fortran ","4.Assembly "}};
+        //DECLARE VARIABLES
+        int[] answers = {3,1,2,4,3};
+        int score = 0;
+        int guess;
+
+        Scanner scanner = new Scanner(System.in);
+        //WELCOME MESSAGE
+
+        System.out.println("******************************");
+        System.out.println("Welcome to the Java Quiz Game!");
+        System.out.println("******************************");
+
+        //QUESTIONS(LOOP)
+
+        for(int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            //  OPTIONS
+            for (String option : options[i]) {
+                System.out.println(option);
+            }
+            //  GET GUESS FROM USER
+            System.out.println("Enter your guess: ");
+            guess = scanner.nextInt();
+            //  CHECK OUR GUESS
+            if (guess == answers[i]) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+        //DISPLAY FINAL SCORE
+        System.out.println("Your score is " + score + " out of " + questions.length);
+        scanner.close();
+    }
+}
